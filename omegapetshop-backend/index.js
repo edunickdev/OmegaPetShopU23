848028c412f12dc231561fcc1cca32b50f6bd2e0
edunickdev <edunsarmiento@soy.sena.edu.co>
2022-10-26 23:19:41 -0500

    Error de conexion con la base de datos corregido

--- omegapetshop-backend/index.js
+++ omegapetshop-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require ('morgan');
 const cors = require('cors');
-const mongoose = require('./conexion.js');
+const mongoose = require("./conexion");
 
 const app = express();
 const env = process.env;
@@ -18,4 +18,4 @@ app.get("/",(request, response)=>{
     response.send("API iniciado para OmegaPetShop Project");
 });
 
-app.use(("/clientes", require("./rutas/clienteRutas")));
+app.use("/clientes", require("./rutas/clienteRutas"));
