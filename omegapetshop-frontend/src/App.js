cbe25ae0c712608aa0b9e251834221de5746f292
edunickdev <edunsarmiento@soy.sena.edu.co>
2022-11-01 22:15:15 -0500

    empezando el frontend

---
+++ omegapetshop-frontend/src/App.js
@@ -0,0 +1,11 @@
+import Carousel from "./general/Carousel";
+import Header from "./general/Header";
+
+const App = () => {
+  return (
+    <Header />,
+    <Carousel />
+  );
+}
+
+export default App;
